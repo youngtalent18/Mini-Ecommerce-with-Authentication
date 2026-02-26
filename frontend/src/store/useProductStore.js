69f7da9389a221c03c8af78b3a81c3830a0bebcb
Stephen Anti <stephenanti63@gmail.com>
2026-02-25 23:03:35 -0800

    1-modified the zustand product store

--- frontend/src/store/useProductStore.js
+++ frontend/src/store/useProductStore.js
@@ -1,5 +1,38 @@
 import {create} from "zustand"
+import axios from "../api/axios.js"
+import {toast} from "react-hot-toast"
 
 export default create((set)=>({
     products: [],
+    loading: false,
+
+    setProducts: (products) => set({products}),
+
+    fetchAllProducts: async () => {
+        set({loading: true});
+        try{
+            const res = await axios.get("/products");
+            set({products: res.data, loading: false});
+        }catch(error){
+            set({error: "Failed to fetch products", laoding: false});
+            toast.error(error.res.data.error || "Failed to fetch products");
+        }
+    },
+    createProducts: async () => {
+        
+    },
+    deleteProducts: async (id) => {
+        try{
+
+        }catch(error){
+
+        }
+    },
+    toggleFeatured: async () => {
+        try {
+
+        }catch(error){
+
+        }
+    },
 }))
