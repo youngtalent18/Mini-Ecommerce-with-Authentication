93f3954ded02e0057435c3606de58b47c06e0fc1
Stephen Anti <stephenanti63@gmail.com>
2026-03-02 19:03:46 -0800

    1-added a limit for large images to be able to be added in cloudinary

--- backend/server.js
+++ backend/server.js
@@ -20,7 +20,7 @@ app.use(cors({
     origin: "http://localhost:5173",
     credentials: true,
 }));
-app.use(express.json());
+app.use(express.json({ limit: "10mb"}));
 app.use(cookieParser());
 app.use(express.urlencoded({extended: true}));
 
