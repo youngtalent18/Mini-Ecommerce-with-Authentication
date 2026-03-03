c398cd23012504973abb52ab9164ac451c9f10aa
Stephen Anti <stephenanti63@gmail.com>
2026-03-03 00:26:31 -0800

    1-fixed a bug thats was preventing product creation

--- frontend/src/api/axios.js
+++ frontend/src/api/axios.js
@@ -1,15 +1,10 @@
-import axios from "axios"
+import axios from "axios";
 
 const api = axios.create({
-    baseURL: "/api",
-    withCredentials: true,
+  baseURL: import.meta.env.DEV
+    ? "http://localhost:6060/api"
+    : "/api",
+  withCredentials: true,
 });
 
 export default api;
-
-
-// To make the link dynamic this method will be the best for me
-// const api = axios.create({
-//    baseURL: import.meta.mode === "development" ? "http://localhost:6060" : "/api",
-//    withCredentials: true,
-// });
