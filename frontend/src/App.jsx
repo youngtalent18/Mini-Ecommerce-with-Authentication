150e33f702d5130977db43d708d5cb9678dae7cc
Stephen Anti <stephenanti63@gmail.com>
2026-03-04 22:35:12 -0800

    1-added a new route

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -7,6 +7,7 @@ import Navbar from './Components/Navbar.jsx'
 import {Toaster } from "react-hot-toast"
 import useStore from './store/useStore.js'
 import { useEffect } from 'react'
+import CategoryPage from './Pages/CategoryPage.jsx'
 
 
 function App() {
@@ -28,6 +29,7 @@ function App() {
           <Route path='/signup' element={!user ? <SignUp /> : <Navigate to="/"/>} />
           <Route path='/login' element={!user ? <LoginPage /> : <Navigate to="/"/> } />
           <Route path='/dashboard' element={user && user?.role === "admin" ? <AdminPage /> : <Navigate to="/login"/>} />
+          <Route path='/category/:category' element={<CategoryPage/>} />
       </Routes>
 
     </div>
