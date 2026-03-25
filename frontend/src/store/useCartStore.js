056b6541da9dd021c1ea94e3a2807800a816d028
Stephen Anti <stephenanti63@gmail.com>
2026-03-25 01:26:47 -0700

    1-cart store

--- frontend/src/store/useCartStore.js
+++ frontend/src/store/useCartStore.js
@@ -1,12 +1,24 @@
 import {create} from "zustand"
+import axios from "../api/axios.js"
+import { toast } from "react-hot-toast"
 
-export const useCartStore = create(()=>({
+export const useCartStore = create((set)=>({
     cart: [],
     total: 0,
     coupon: null,
     subTotal: 0,
 
     getCartItems: async () => {
+        try{
+            const res = await axios.get("/cart");
+            set({cart: res.data});
+        }catch(error){
+            set({cart: []});
+            const message = error.response?.data?.message || "Failed to fetch cart items";
+            toast.error(message);
+        }
+    },
+    addToCart: async () => {
 
-    }
+    },
 }))
