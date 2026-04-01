95c2fdc125e5820f8d35008043b370bd6d80e148
Stephen Anti <stephenanti63@gmail.com>
2026-03-31 17:51:39 -0700

    1-error fix

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -8,14 +8,22 @@ import {Toaster } from "react-hot-toast"
 import useStore from './store/useStore.js'
 import { useEffect } from 'react'
 import CategoryPage from './Pages/CategoryPage.jsx'
+import CartPage from './Pages/CartPage.jsx'
+import { useCartStore } from './store/useCartStore.js'
 
 
 function App() {
 
   const {user, checkAuth, checkingAuth} = useStore();
+  const { getCartItems } = useCartStore();
+  
   useEffect(()=>{
     checkAuth()
   },[checkAuth]);
+
+  useEffect(()=>{
+    getCartItems()
+  },[getCartItems]);
 
   if (checkingAuth) return <div className='loader'>Loading...</div>;
 
@@ -30,6 +38,7 @@ function App() {
           <Route path='/login' element={!user ? <LoginPage /> : <Navigate to="/"/> } />
           <Route path='/dashboard' element={user && user?.role === "admin" ? <AdminPage /> : <Navigate to="/login"/>} />
           <Route path='/category/:category' element={<CategoryPage/>} />
+          <Route path='/cart' element={ user? <CartPage/>:<Navigate to="/login"/>} />
       </Routes>
 
     </div>
