f90a666a53cd1833a4213731bca1b165df5c9b8f
Stephen Anti <stephenanti63@gmail.com>
2026-04-01 22:45:09 -0700

    1-new feature

--- frontend/src/Pages/CartPage.jsx
+++ frontend/src/Pages/CartPage.jsx
@@ -1,8 +1,31 @@
 import React from 'react'
-
+import { useCartStore } from '../store/useCartStore.js';
+import PeopleAlsoBought from '../Components/PeopleAlsoBought.jsx';
 const CartPage = () => {
+  const {cart} = useCartStore();
   return (
-    <div>CartPage</div>
+    <div className='cart-main'>
+      <div className='cart-sub'>
+        <div className="cart-sub-left">
+          <motion.div className='cart-items-con' initial={{x:100, opacity:0}} animate={{x:0, opacity:1}} transition={{duration:0.5}}>
+            {cart.length > 0 ? cart.map(item=>(
+              <div className='cart-item' key={item._id}>
+                <img src={item.image} alt={item.name} />
+                <div className='cart-item-details'>
+                  <h3>{item.name}</h3>
+                  <p>${item.price.toFixed(2)}</p>
+                  <p>Quantity: {item.quantity}</p>
+                </div>
+              </div>
+            )) : (
+              <p>Your cart is empty</p>
+            )}
+            {cart.length > 0 && <PeopleAlsoBought />}
+          </motion.div>
+
+        </div>
+      </div>
+    </div>
   )
 }
 
